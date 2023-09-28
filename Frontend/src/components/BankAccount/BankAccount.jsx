b2fc492f1646d8ed6c78344569940e0814a5ed22
SebMZI <mrgy.sebastien@gmail.com>
2023-09-28 18:49:03 +0200

    feat: Responsive phone/tablette Add responsive for screens, modal is responsive

--- Frontend/src/components/BankAccount/BankAccount.jsx
+++ Frontend/src/components/BankAccount/BankAccount.jsx
@@ -54,7 +54,7 @@ const BankAccount = ({ id, balance, accId }) => {
             <p className="account-balance">${balance}</p>
             <p className="account-subtitle">Available balance</p>
           </div>
-          <p className="chevron">{toggle ? "X" : ">"}</p>
+          <p className="chevron">{toggle ? "x" : ">"}</p>
         </article>
         {client ? null : (
           <div>
